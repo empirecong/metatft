14800b088d049808d38dcb25c75b5afea5bc629d
EmpireCong <63647753+EmpireCong@users.noreply.github.com>
2026-05-15 03:05:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 
-from backend.meta import get_meta
+from meta import get_meta
 
 TOKEN = os.getenv("BOT_TOKEN")
 
